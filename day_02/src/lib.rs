417ec31f0073d755c1c58a5fa1e2896cfa90ed0d
Ivan Vukman <ivan.vukman@gmail.com>
2023-12-17 01:29:05 +0100

    Solve day 2 part 2

--- day_02/src/lib.rs
+++ day_02/src/lib.rs
@@ -1,4 +1,6 @@
-pub fn calculate(input: &str) -> usize {
+use std::cmp;
+
+pub fn calculate_part_1(input: &str) -> usize {
     input
         .lines()
         .enumerate()
@@ -22,4 +24,37 @@ pub fn calculate(input: &str) -> usize {
                 .then_some(i + 1)
         })
         .sum()
+}
+
+pub fn calculate_part_2(input: &str) -> usize {
+    input
+        .lines()
+        .map(|line| {
+            let (r, g, b) = line
+                .splitn(2, ':')
+                .nth(1)
+                .unwrap()
+                .split(|l| l == ',' || l == ';')
+                .map(|game_move| {
+                    let mut split = game_move.trim().splitn(2, ' ');
+                    let num = split.nth(0).unwrap().parse::<usize>().unwrap();
+                    let color = split.nth(0).unwrap();
+
+                    match color {
+                        "red" => (num, 0, 0),
+                        "green" => (0, num, 0),
+                        "blue" => (0, 0, num),
+                        _ => (0, 0, 0),
+                    }
+                })
+                .fold((0, 0, 0), |acc, e| {
+                    (
+                        cmp::max(acc.0, e.0),
+                        cmp::max(acc.1, e.1),
+                        cmp::max(acc.2, e.2),
+                    )
+                });
+            r * g * b
+        })
+        .sum::<usize>()
 }
--- day_02/tests/integration_tests.rs
+++ day_02/tests/integration_tests.rs
@@ -1,13 +1,29 @@
 #[test]
 fn test_part_1_sample() {
-    let input = include_str!("../input/part_1_sample.txt");
-    let result = day_02::calculate(input);
+    let input = include_str!("../input/sample.txt");
+    let result = day_02::calculate_part_1(input);
     assert_eq!(8, result);
 }
 
 #[test]
 fn test_part_1() {
-    let input = include_str!("../input/part_1.txt");
-    let result = day_02::calculate(input);
+    let input = include_str!("../input/puzzle_input.txt");
+    let result = day_02::calculate_part_1(input);
     assert_eq!(2771, result);
+}
+
+#[test]
+fn test_part_2_sample()
+{
+    let input = include_str!("../input/sample.txt");
+    let result = day_02::calculate_part_2(input);
+    assert_eq!(2286, result);
+}
+
+#[test]
+fn test_part_2()
+{
+    let input = include_str!("../input/puzzle_input.txt");
+    let result = day_02::calculate_part_2(input);
+    assert_eq!(70924, result);
 }
